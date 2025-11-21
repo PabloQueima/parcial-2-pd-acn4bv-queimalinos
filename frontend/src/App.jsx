d8ba206edf110101316906dae3eed47a777a12ec
Pablo Queimaliños <pablo.queimalinos@davinci.edu.ar>
2025-11-21 23:53:52 +0000

    fix: login entrenador / cliente

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import "./styles/styles.css";
 
 import DashboardAdmin from "./pages/DashboardAdmin";
 import DashboardEntrenador from "./pages/DashboardEntrenador";
 import DashboardCliente from "./pages/DashboardCliente";
+
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 
@@ -10,7 +14,17 @@ import Navbar from "./components/Navbar";
 import { getCurrentUser } from "./services/authService";
 
 export default function App() {
-  const user = getCurrentUser();
+  const [currentUser, setCurrentUser] = useState(getCurrentUser());
+
+  useEffect(() => {
+    // Observa si el usuario cambia en localStorage (cuando haces login)
+    const interval = setInterval(() => {
+      const stored = getCurrentUser();
+      setCurrentUser(stored);
+    }, 300);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <>
@@ -18,13 +32,12 @@ export default function App() {
 
       <Routes>
         <Route path="/" element={<LandingPage />} />
-
         <Route path="/login" element={<LoginPage />} />
 
         <Route
           path="/admin"
           element={
-            user?.rol === "admin"
+            currentUser?.rol === "admin"
               ? <DashboardAdmin />
               : <Navigate to="/login" />
           }
@@ -33,7 +46,7 @@ export default function App() {
         <Route
           path="/entrenador"
           element={
-            user?.rol === "entrenador"
+            currentUser?.rol === "entrenador"
               ? <DashboardEntrenador />
               : <Navigate to="/login" />
           }
@@ -42,7 +55,7 @@ export default function App() {
         <Route
           path="/cliente"
           element={
-            user?.rol === "cliente"
+            currentUser?.rol === "cliente"
               ? <DashboardCliente />
               : <Navigate to="/login" />
           }
